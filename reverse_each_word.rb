695da46f2e09aabc4e5a6d884687d75d9af76947
Stacey P Carrillo <staceycarrillo44@gmail.com>
2020-09-05 02:49:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 reverse_each_word(string)
   new_array = string.split("")
   last_array = []
-  new_array.reverse_each do |x|
+  new_array.reverse_each_word do |x|
     last_array.push(x)
   end
   last_array.join
